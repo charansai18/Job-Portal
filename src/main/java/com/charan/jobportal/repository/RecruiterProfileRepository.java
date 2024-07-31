fe1cd632c0de0ad18b9e25020e324e5f92688c1e
Charan Sai <acs13@iitbbs.ac.in>
2024-07-31 19:55:26 +0530

    initial commit

---
+++ src/main/java/com/charan/jobportal/repository/RecruiterProfileRepository.java
@@ -0,0 +1,7 @@
+package com.charan.jobportal.repository;
+
+import com.charan.jobportal.entity.RecruiterProfile;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RecruiterProfileRepository extends JpaRepository<RecruiterProfile, Integer> {
+}
---
+++ src/main/java/com/charan/jobportal/services/UsersTypeService.java
@@ -0,0 +1,21 @@
+package com.charan.jobportal.services;
+
+import com.charan.jobportal.entity.UsersType;
+import com.charan.jobportal.repository.UsersTypeRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UsersTypeService {
+
+    private final UsersTypeRepository usersTypeRepository;
+
+    public UsersTypeService(UsersTypeRepository usersTypeRepository) {
+        this.usersTypeRepository = usersTypeRepository;
+    }
+
+    public List<UsersType> getAll() {
+        return usersTypeRepository.findAll();
+    }
+}
